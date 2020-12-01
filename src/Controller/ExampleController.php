a2d1ca9c79ca464ab5d4b14addeca9a967895292
LL\piefady <piefady@publicisgroupe.net>
2020-12-01 14:28:30 +0100

    part 1

---
+++ src/Controller/ExampleController.php
@@ -0,0 +1,21 @@
+<?php
+  namespace Drupal\adventofcode\Controller;
+
+  use Drupal\Core\Controller\ControllerBase;
+
+  /**
+   * An example controller.
+   */
+  class ExampleController extends ControllerBase {
+
+    /**
+     * Returns a render-able array for a test page.
+     */
+    public function content() {
+      $build = [
+        '#markup' => $this->t('Hello World!'),
+      ];
+      return $build;
+    }
+
+  }
